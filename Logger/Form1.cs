68aeda69adde31e8f6562ab7a655015fb4b53ef8
M4nuski <the_man994@hotmail.com>
2015-03-25 01:49:53 -0400

    initial commit with basic Logger Component

---
+++ Logger/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Logger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Logger.AddLine(textBox1.Text);
+                textBox1.Clear();
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Logger.AddText(textBox2.Text);
+                textBox2.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Logger.Clear();
+        }
+    }
+}
---
+++ Logger/LoggerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows.Forms;
+
+namespace Logger
+{
+    public partial class LoggerControl : UserControl
+    {
+        private StringBuilder _builder = new StringBuilder();
+        //TODO add logfile
+        //TODO add timestamps
+
+        public LoggerControl()
+        {
+            InitializeComponent();
+        }
+
+        public void AddLine(string text)
+        {
+            _builder.AppendLine(text);
+            OutputTextBox.Text = _builder.ToString();
+        }
+
+        public void AddText(string text)
+        {
+            _builder.Append(text);
+            OutputTextBox.Text = _builder.ToString();
+        }
+
+        public void Clear()
+        {
+            _builder.Clear();
+            OutputTextBox.Clear();
+        }
+
+
+    }
+}
